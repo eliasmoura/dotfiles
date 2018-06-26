e311ace976ad41f86db9e72d784b974c8c9de297
Elias Alves Moura <eliasmoura.alves@gmail.com>
2018-06-27 00:11:04 +0100

    basic config

--- local/cfg/vis/visrc.lua
+++ local/cfg/vis/visrc.lua
@@ -1,5 +1,15 @@
 -- load standard vis module, providing parts of the Lua API
 require('vis')
+require('plugins/filetype')
+require('plugins/complete-filename')
+require('plugins/complete-word')
+
+local home = os.getenv('XDG_CONFIG_HOME')
+local cfg       = home .. '/vis'
+package.path = package.path .. ';' .. cfg .. '/plugins/?'
+require('plugins/vis-commentary/vis-commentary')
+require('plugins/vis-surround/vis-surround')
+require('plugins/vis-git-status/vis-git-status')
 
 vis.events.subscribe(vis.events.INIT, function()
 	-- Your global configuration options
@@ -12,7 +22,11 @@ vis.events.subscribe(vis.events.WIN_OPEN, function(win)
 	vis:command('set cursorline')
 	vis:command('set autoindent')
 	vis:command('set tabwidth 4')
-	vis:command('set expandtab')
+	if win.syntax ~= 'makefile' then
+    	vis:command('set expandtab')
+    else
+    	vis:command('set expandtab off')
+	end
 	vis:command('set show-tabs')
 	vis:command('set show-spaces')
 	vis:command('set show-newlines')
