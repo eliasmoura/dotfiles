a8c6d1bb4a1e52ad534b9488d892b537c8faa6bb
Elias Alves Moura <eliasmoura.alves@gmail.com>
2021-09-09 16:02:33 +0100

    nvim: use nvim/init.lua instead of nvim/lua/custom/init.lua.

--- local/cfg/nvim/init.lua
+++ local/cfg/nvim/init.lua
@@ -1,5 +1,44 @@
---
--- Nothing to see here.
---
+local cfg = vim.fn.stdpath('config')
+local data = vim.fn.stdpath('data')
+local cache = vim.fn.stdpath('cache')
+local backup = data .. '/backup/'
+vim.custom = {}
+if not vim.fn.isdirectory(backup) then
+	  vim.fn.mkdir(backup, 'p')
+  end
+vim.g.cfg = cfg
+vim.g.data = data
+vim.g.cache = cache
 
-require('custom')
+require('custom.sets')
+require('custom.plugins')
+
+vim.custom.toggleBackground = function()
+  if vim.o.background == "light" then
+    vim.o.background = 'dark'
+  else
+    vim.o.background = 'light'
+  end
+end
+
+-- NOTE(kotto): Kinda defer the loading of the mappings so it doens't take 400s to load
+vim.schedule(function ()
+  vim.custom.mappings = require('custom.mappings')
+  vim.keymap.nnoremap{'<silent> <leader>'     , ':<c-u>WhichKey <\\Space><CR>'}
+  vim.keymap.nnoremap{'<silent> <localleader>',  ':<c-u>WhichKey  ,<CR>'}
+  require('custom.lsp')
+end)
+
+vim.cmd[[autocmd BufEnter * :LspStart]]
+vim.cmd [[runtime autoload/auto_stuff.vim]]
+vim.g.tokyonight_style = 'night'
+vim.cmd [[colorscheme tokyonight]]
+
+vim.g.vimspector_enable_mappings = 'HUMAN'
+
+vim.g.tar_cmd = 'bsdtar'
+vim.g.tar_secure = 1
+
+-- Tell vim about the additional file extensions we can now use.
+vim.cmd [[autocmd BufReadCmd *.iso,*.rar,*.7z call tar#Browse(expand("<tch>"))]]
+
