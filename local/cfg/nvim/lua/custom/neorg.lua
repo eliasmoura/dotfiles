e8110df6499234a49c57bf6ff3825ad782163fa2
Elias Alves Moura <eliasmoura.alves@gmail.com>
2021-09-09 16:12:03 +0100

    nvim: neorg a Org mode like for vim.

---
+++ local/cfg/nvim/lua/custom/neorg.lua
@@ -0,0 +1,28 @@
+local norg = require('neorg')
+print('NORG!')
+
+local parser_configs = require('nvim-treesitter.parsers').get_parser_configs()
+
+parser_configs.norg = {
+    install_info = {
+        url = "https://github.com/vhyrro/tree-sitter-norg",
+        files = { "src/parser.c", "src/scanner.cc" },
+        branch = "main"
+    },
+}
+
+norg.setup{
+  load = {
+    ["core.defaults"] = {}, -- Load all the default modules
+    ["core.norg.concealer"] = {}, -- Allows for use of icons
+    ["core.integrations.treesitter"] = {},
+    ["core.integrations.telescope"] = {},
+    ["core.norg.dirman"] = { -- Manage your directories with Neorg
+        config = {
+            workspaces = {
+                main = "~/writings/notes/"
+            }
+        }
+    }
+  },
+}
