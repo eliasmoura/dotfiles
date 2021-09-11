226ee2637bf46962960de0d372003fbe233246a5
Elias Alves Moura <eliasmoura.alves@gmail.com>
2021-09-11 18:02:21 +0100

    nvim: remove unused code. Trying out whichkey.

--- local/cfg/nvim/lua/custom/mappings.lua
+++ local/cfg/nvim/lua/custom/mappings.lua
@@ -1,17 +1,13 @@
 local nnoremap = vim.keymap.nnoremap
 local inoremap = vim.keymap.inoremap
--- local xnoremap = vim.keymap.xnoremap
 local vnoremap = vim.keymap.vnoremap
 local tnoremap = vim.keymap.tnoremap
--- local nmap = vim.keymap.nmap
--- local imap = vim.keymap.imap
--- local xmap = vim.keymap.xmap
--- local vmap = vim.keymap.vmap
--- local tmap = vim.keymap.tmap
--- local noremap = vim.keymap.noremap
 local map = vim.keymap.map
+local wk = require('whichkey_setup')
+local leader_map = {}
+local M = {}
+-- local xnoremap = vim.keymap.xnoremap
 
-vim.api.nvim_set_keymap('', ' ', '', {})
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ','
 
@@ -19,6 +15,7 @@ map{'Q', 'gq'}
 nnoremap{'<leader>rt', [[:%s/\s\+$//g<cr>:let @/=''<cr>]]}
 nnoremap{'<leader>/', ':silent :nohlsearch<CR>:<CR>'}
 nnoremap{'<leader><Tab>', ':silent :retab!<CR>'}
+leader_map['/'] = 'Remove search highlight'
 
 -- Git
 nnoremap{'<leader>gg', ':Git '}
@@ -64,34 +61,16 @@ nnoremap{'<leader>th',      tl.help_tags}
 nnoremap{'<leader><space>', tl.find_files}
 nnoremap{'<leader>rr',      tl.reloader}
 
--- vim.cmd [[runtime plugin/vimspector.vim]]
--- Vimspector
--- nnoremap{'<F9>', vim.fn['vimspector#ToggleBreakpoint']}
--- nnoremap{'<F5>', vim.fn['vimspector#Continue']}--	When debugging, continue. Otherwise start debugging.
--- nnoremap{'<leader>vl', vim.fn['vimspector#Launch']}--	When debugging, continue. Otherwise start debugging.
--- nnoremap{'<F3>', vim.fn['vimspector#Stop']}--	Stop debugging.
--- nnoremap{'<F4>', vim.fn['vimspector#Restart']}--	Restart debugging with the same configuration.
--- nnoremap{'<F6>', vim.fn['vimspector#Pause']}--	Pause debuggee.
--- nnoremap{'<F9>', vim.fn['vimspector#ToggleBreakpoint']}--	Toggle line breakpoint on the current line.
--- nnoremap{'<leader><F9>', vim.fn['vimspector#ToggleConditionalBreakpoint']}--	Toggle conditional line breakpoint on the current line.
--- nnoremap{'<F8>', vim.fn['vimspector#AddFunctionBreakpoint']}--	Add a function breakpoint for the expression under cursor
--- nnoremap{'<leader><F8>', vim.fn['vimspector#RunToCursor']}--	Run to Cursor
--- nnoremap{'<F10>', vim.fn['vimspector#StepOver']}--	Step Over
--- nnoremap{'<F11>', vim.fn['vimspector#StepInto']}--	Step Into
--- nnoremap{'<F12>', vim.fn['vimspector#StepOut']}--	Step out of current function scope
--- nnoremap{'<F1>', vim.fn['vimspector#Reset']}--	Step out of current function scope
 nnoremap{'<leader>db', require('dap').toggle_breakpoint}
--- nnoremap{'<leader>dB', require('dap').set_breackpoint}
 nnoremap{'<leader>dC', require('dap').continue}--	when debugging, continue. otherwise start debugging.
 nnoremap{'<leader>dc', require('dap').run_to_cursor}--	when debugging, continue. otherwise start debugging.
--- nnoremap{'<leader>dL', require('dap').launch}--	when debugging, continue. otherwise start debugging.
 nnoremap{'<leader>d<space>', require('dap').stop}--	stop debugging.
 nnoremap{'<leader>dr', require('dap').run}--	restart debugging with the same configuration.
 nnoremap{'<leader>dR', require('dap').restart}--	restart debugging with the same configuration.
 nnoremap{'<leader>dP', require('dap').pause}--	pause debuggee.
 nnoremap{'<leader>d<space>', require('dap').run_to_cursor}--	run to cursor
-nnoremap{'<leader>dS', require('dap').step_over}--	step over
-nnoremap{'<leader>ds', require('dap').step_into}--	step into
+nnoremap{'<leader>ds', require('dap').step_over}--	step over
+nnoremap{'<leader>dS', require('dap').step_into}--	step into
 nnoremap{'<F5>',       require('dap').step_into}--	step into
 nnoremap{'<leader>do', require('dap').step_out}--	step out of current function scope
 nnoremap{'<leader>du', require('dap').up}--	step out of current function scope
