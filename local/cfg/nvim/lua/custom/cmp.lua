f4a59597d83dd66b43d85db06edacfa95868fc08
Elias Alves Moura <eliasmoura.alves@gmail.com>
2021-10-27 15:30:03 +0100

    nvim(cmp): fix mappings add ghost_exmark.

--- local/cfg/nvim/lua/custom/cmp.lua
+++ local/cfg/nvim/lua/custom/cmp.lua
@@ -1,25 +1,14 @@
-local has_words_before = function()
-  if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then
-    return false
-  end
-  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0
-    and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]
-        :sub(col, col)
-        :match("%s")
-      == nil
+
+local ok1, luasnip = pcall(require, "luasnip")
+local ok2, cmp = pcall(require, "cmp")
+
+if not ok1 or not ok2 then
+  return
 end
 
-local feedkey = function(key)
-  vim.api.nvim_feedkeys(
-    vim.api.nvim_replace_termcodes(key, true, true, true),
-    "n",
-    true
-  )
+function feedkey(key)
+  vim.api.nvim_replace_termcodes(key, true, true, true)
 end
-
-local luasnip = require("luasnip")
-local cmp = require("cmp")
 
 cmp.setup({
   snippet = {
@@ -27,41 +16,31 @@ cmp.setup({
       require("luasnip").lsp_expand(args.body)
     end,
   },
-  mappings = {
+  mapping = {
     ["<C-d>"] = cmp.mapping.scroll_docs(-4),
     ["<C-f>"] = cmp.mapping.scroll_docs(4),
     ["<C-Space>"] = cmp.mapping.complete(),
     ["<C-e>"] = cmp.mapping.close(),
-    ["<CR>"] = cmp.mapping.confirm({
-      behavior = cmp.ConfirmBehavior.Insert,
-      select = true,
-    }),
-    ["<m-s>"] = cmp.mapping(function(fallback)
-      if vim.fn.pumvisible() == 1 then
-        P("HERE1")
-        feedkey("<C-n>")
-      elseif luasnip.expand_or_jumpable() then
-        P("HERE2")
+    ["<c-i>"] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Insert, select = true }) ,
+    ["<c-n>"] = cmp.mapping(function()
+      if cmp and cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip and luasnip.expand_or_jumpable() then
         luasnip.expand_or_jump()
-      elseif has_words_before() then
-        P("HERE3")
-        cmp.complete()
       else
-        P("HERE4")
-        -- fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
+        cmp.complete()
       end
     end, {
       "i",
       "s",
     }),
-
-    ["<m-s-s>"] = cmp.mapping(function(fallback)
-      if vim.fn.pumvisible() == 1 then
-        feedkey("<C-p>")
-      elseif luasnip.jumpable(-1) then
+    ["<c-p>"] = cmp.mapping(function()
+      if cmp and cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip and luasnip.jumpable(-1) then
         luasnip.jump(-1)
       else
-        fallback()
+        cmp.complete()
       end
     end, {
       "i",
@@ -69,10 +48,14 @@ cmp.setup({
     }),
   },
   sources = {
-    { name = "nvim_lsp" },
-    { name = "luasnip" },
-    { name = "nvim_lua" },
-    { name = "buffer" },
-    { name = "path" },
+    { name = "nvim_lsp", priority = 10 },
+    { name = "nvim_lua", priority = 6 },
+    { name = "luasnip", priority = 3 },
+    { name = "buffer", priority = 4 },
+    { name = "path", priority = 2 },
   },
+  experimental = {
+    native_menu = false,
+    ghost_text = true,
+    }
 })
