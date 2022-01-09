a682381adb4f422563ebe8e7d797e125c64a3ec8
Elias Alves Moura <eliasmoura.alves@gmail.com>
2022-01-09 12:04:11 +0000

    update(nvim): use the new keymap api.

--- local/cfg/nvim/lua/custom/mappings.lua
+++ local/cfg/nvim/lua/custom/mappings.lua
@@ -1,21 +1,18 @@
-local nnoremap = vim.keymap.nnoremap
-local inoremap = vim.keymap.inoremap
-local vnoremap = vim.keymap.vnoremap
-local tnoremap = vim.keymap.tnoremap
-local map = vim.keymap.map
 local M = {}
 -- local xnoremap = vim.keymap.xnoremap
 
 vim.g.mapleader = " "
 vim.g.maplocalleader = ","
 
-nnoremap({ "<space>", "<nop>" })
-nnoremap({ "<cr>", "<nop>" })
-map({ "Q", "gq" })
-nnoremap({ "<leader>rt", [[<cmd>%s/\s\+$//g<cr>let @/=''<cr>]] })
-nnoremap({ "<cr>", vim.custom.nohl })
-nnoremap({ "<leader>/", "<cmd>nohlsearch<CR><cmd>echo<cr>" }) -- teh second cmd is to remove the search from the echo area
-nnoremap({ "<leader><Tab>", "<cmd>retab!<CR>" })
+vim.keymap.set("n", "<space>", "<nop>")
+vim.keymap.set("n", "<cr>", "<nop>")
+vim.keymap.set("n", "<c-n>", "<nop>")
+vim.keymap.set("n", "<c-p>", "<nop>")
+vim.keymap.set("n", "Q", "gq")
+vim.keymap.set("n", "<leader>rt", [[<cmd>%s/\s\+$//g<cr>let @/=''<cr>]])
+vim.keymap.set("n", "<cr>", vim.custom.nohl)
+vim.keymap.set("n", "<leader>/", "<cmd>nohlsearch<cr><cmd>echo<cr>") -- teh second cmd is to remove the search from the echo area
+vim.keymap.set("n", "<leader><tab>", "<cmd>retab!<cr>")
 
 -- Git
 local neogit = {}
@@ -31,85 +28,77 @@ end
 function neogit.status()
   require("neogit").open({ kind = "split" })
 end
-nnoremap({ "<leader>gg", "<cmd>Neogit<cr>" })
--- nnoremap({ "<leader>gg", neogit.status })
--- nnoremap({ "<leader>gG", ":Git! " })
--- nnoremap({ "<leader>gs", require("neogit").cr>" })
-nnoremap({ "<leader>gc", neogit.commit })
-nnoremap({ "<leader>gm", neogit.merge })
-nnoremap({ "<leader>gl", neogit.log })
+vim.keymap.set("n", "<leader>gg", "<cmd>Neogit<cr>")
+-- vim.keymap.set('n', "<leader>gg", neogit.status )
+-- vim.keymap.set('n', "<leader>gG", ":Git! " )
+-- vim.keymap.set('n', "<leader>gs", require("neogit").cr>" )
+vim.keymap.set("n", "<leader>gc", neogit.commit)
+vim.keymap.set("n", "<leader>gm", neogit.merge)
+vim.keymap.set("n", "<leader>gl", neogit.log)
 
 -- Lsp
 -- For the Lsp mappings see lua/custom/lsp.lua
-nnoremap({ "<c-n>", vim.diagnostic.goto_next })
-nnoremap({ "<c-p>", vim.diagnostic.goto_prev })
+vim.keymap.set("n", "<c-n>", vim.diagnostic.goto_next)
+vim.keymap.set("n", "<c-p>", vim.diagnostic.goto_prev)
 
 -- NOTE(elias): It seems like diagnostic.show_line_diagnostics is buggy now
--- vim.diagnostic.show_line_diagnostics = function()
---   local namespaces = vim.api.nvim_get_namespaces()
---   -- P(namespaces)
---   local l_num = vim.api.nvim_win_get_cursor(vim.api.nvim_get_current_win())[1]
---   local diagnostics = vim.diagnostic.get(0, { lnum = l_num })
+-- vim.keymap.set('n', "<c-s>", vim.diagnostic.show_line_diagnostics )
 
---   vim.diagnostic.show(namespaces["vim.lsp.client-1"], 0, diagnostics, nil)
--- end
--- nnoremap({ "<c-s>", vim.diagnostic.show_line_diagnostics })
+vim.keymap.set("n", "<m-m>", "<cmd>make clean all<cr>")
+vim.keymap.set("n", "<m-s-m>", ":make ")
+vim.keymap.set("t", "<esc>", "<c-\\><c-n>")
+vim.keymap.set("n", "<esc>", "<c-\\><c-n>")
+vim.keymap.set("n", "<c-s-a>", "<c-^>")
 
-nnoremap({ "<m-m>", "<cmd>make clean all<CR>" })
-nnoremap({ "<m-s-m>", ":make " })
-tnoremap({ "<Esc>", "<C-\\><C-n>" })
-nnoremap({ "<Esc>", "<C-\\><C-n>" })
-nnoremap({ "<C-S-a>", "<C-^>" })
-
-nnoremap({ "<M-c>", "<cmd>lclose<cr>" })
-nnoremap({ "<M-o>", "<cmd>lopen<cr>" })
-nnoremap({ "<m-n>", "<cmd>lnext<cr>" })
-nnoremap({ "<m-p>", "<cmd>lprev<cr>" })
-nnoremap({ "<m-s-c>", "<cmd>cclose<cr>" })
-nnoremap({ "<m-s-o>", "<cmd>copen<cr>" })
-nnoremap({ "<m-s-n>", "<cmd>cnext<cr>" })
-nnoremap({ "<m-s-p>", "<cmd>cprev<cr>" })
+vim.keymap.set("n", "<M-c>", "<cmd>lclose<cr>")
+vim.keymap.set("n", "<M-o>", "<cmd>lopen<cr>")
+vim.keymap.set("n", "<m-n>", "<cmd>lnext<cr>")
+vim.keymap.set("n", "<m-p>", "<cmd>lprev<cr>")
+vim.keymap.set("n", "<m-s-c>", "<cmd>cclose<cr>")
+vim.keymap.set("n", "<m-s-o>", "<cmd>copen<cr>")
+vim.keymap.set("n", "<m-s-n>", "<cmd>cnext<cr>")
+vim.keymap.set("n", "<m-s-p>", "<cmd>cprev<cr>")
 
 --Telescope
 local tl = require("custom.telescope")
-nnoremap({ "<leader>tt", tl.builtin })
-nnoremap({ "<leader>ti", tl.edit_nvim })
-nnoremap({ "<leader>tcf", tl.edit_cfg })
-nnoremap({ "<leader>tg", tl.live_grep })
-nnoremap({ "<leader>tb", tl.buffers })
-nnoremap({ "<leader>tf", tl.navigate })
-nnoremap({ "<leader>tm", tl.man_pages })
-nnoremap({ "<leader>tcs", tl.colorscheme })
-nnoremap({ "<leader>tq", tl.quickfix })
-nnoremap({ "<leader>tl", tl.loclist })
-nnoremap({ "<leader>tss", tl.spell_suggest })
-nnoremap({ "<leader>th", tl.help_tags })
-nnoremap({ "<leader><space>", tl.find_files })
-nnoremap({ "<leader>rr", tl.reloader })
+vim.keymap.set("n", "<leader>tt", tl.builtin)
+vim.keymap.set("n", "<leader>ti", tl.edit_nvim)
+vim.keymap.set("n", "<leader>tcf", tl.edit_cfg)
+vim.keymap.set("n", "<leader>tg", tl.live_grep)
+vim.keymap.set("n", "<leader>tb", tl.buffers)
+vim.keymap.set("n", "<leader>tf", tl.navigate)
+vim.keymap.set("n", "<leader>tm", tl.man_pages)
+vim.keymap.set("n", "<leader>tcs", tl.colorscheme)
+vim.keymap.set("n", "<leader>tq", tl.quickfix)
+vim.keymap.set("n", "<leader>tl", tl.loclist)
+vim.keymap.set("n", "<leader>tss", tl.spell_suggest)
+vim.keymap.set("n", "<leader>th", tl.help_tags)
+vim.keymap.set("n", "<leader><space>", tl.find_files)
+vim.keymap.set("n", "<leader>rr", tl.reloader)
 
-nnoremap({ "<leader>db", require("dap").toggle_breakpoint })
-nnoremap({ "<leader>db", require("dap").toggle_breakpoint })
-nnoremap({ "<leader>dC", require("dap").continue }) --	when debugging, continue. otherwise start debugging.
-nnoremap({ "<leader>dc", require("dap").run_to_cursor }) --	when debugging, continue. otherwise start debugging.
-nnoremap({ "<leader>d<space>", require("dap").stop }) --	stop debugging.
-nnoremap({ "<leader>dr", require("dap").run }) --	restart debugging with the same configuration.
-nnoremap({ "<leader>dR", require("dap").restart }) --	restart debugging with the same configuration.
-nnoremap({ "<leader>dP", require("dap").pause }) --	pause debuggee.
-nnoremap({ "<leader>d<space>", require("dap").run_to_cursor }) --	run to cursor
-nnoremap({ "<leader>ds", require("dap").step_over }) --	step over
-nnoremap({ "<leader>dS", require("dap").step_into }) --	step into
-nnoremap({ "<F5>", require("dap").step_into }) --	step into
-nnoremap({ "<leader>do", require("dap").step_out }) --	step out of current function scope
-nnoremap({ "<leader>du", require("dap").up }) --	step out of current function scope
-nnoremap({ "<leader>dd", require("dap").down }) --	step out of current function scope
-nnoremap({ "<leader>td", require("dap-go").debug_test })
-nnoremap({ "<leader>dj", "<cmd>call v:lua.dap_prompt_expr()<cr>" })
+vim.keymap.set("n", "<leader>db", require("dap").toggle_breakpoint)
+vim.keymap.set("n", "<leader>db", require("dap").toggle_breakpoint)
+vim.keymap.set("n", "<leader>dC", require("dap").continue) --	when debugging, continue. otherwise start debugging.
+vim.keymap.set("n", "<leader>dc", require("dap").run_to_cursor) --	when debugging, continue. otherwise start debugging.
+vim.keymap.set("n", "<leader>d<space>", require("dap").stop) --	stop debugging.
+vim.keymap.set("n", "<leader>dr", require("dap").run) --	restart debugging with the same configuration.
+vim.keymap.set("n", "<leader>dR", require("dap").restart) --	restart debugging with the same configuration.
+vim.keymap.set("n", "<leader>dP", require("dap").pause) --	pause debuggee.
+vim.keymap.set("n", "<leader>d<space>", require("dap").run_to_cursor) --	run to cursor
+vim.keymap.set("n", "<leader>ds", require("dap").step_over) --	step over
+vim.keymap.set("n", "<leader>dS", require("dap").step_into) --	step into
+vim.keymap.set("n", "<F5>", require("dap").step_into) --	step into
+vim.keymap.set("n", "<leader>do", require("dap").step_out) --	step out of current function scope
+vim.keymap.set("n", "<leader>du", require("dap").up) --	step out of current function scope
+vim.keymap.set("n", "<leader>dd", require("dap").down) --	step out of current function scope
+vim.keymap.set("n", "<leader>td", require("dap-go").debug_test)
+vim.keymap.set("n", "<leader>dj", "<cmd>call v:lua.dap_prompt_expr()<cr>")
 
-nnoremap({ "<leader>tB", vim.custom.toggleBackground })
-nnoremap({ "cu", ":UndotreeToggle<CR>" })
-nnoremap({ "gl", ":EasyAlign<CR>" })
-vnoremap({ "gl", ":EasyAlign<CR>" })
+vim.keymap.set("n", "<leader>tB", vim.custom.toggleBackground)
+vim.keymap.set("n", "cu", ":UndotreeToggle<cr>")
+vim.keymap.set("n", "gl", ":EasyAlign<cr>")
+vim.keymap.set("v", "gl", ":EasyAlign<cr>")
 
-nnoremap({ "<leader>ng", require("neogen").generate })
+vim.keymap.set("n", "<leader>ng", require("neogen").generate)
 
 return M
