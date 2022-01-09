eb28bc5086bbd1425f6c8d12c8a41c63bd959348
Elias Alves Moura <eliasmoura.alves@gmail.com>
2022-01-09 12:02:19 +0000

    cleanup(nvim): clean the config.

--- local/cfg/nvim/init.lua
+++ local/cfg/nvim/init.lua
@@ -1,16 +1,12 @@
 pcall(require, "impatient")
-local cfg = vim.fn.stdpath("config")
 local data = vim.fn.stdpath("data")
-local cache = vim.fn.stdpath("cache")
 local backup = data .. "/backup/"
 local fn = vim.fn
 vim.custom = {}
 if not vim.fn.isdirectory(backup) then
   vim.fn.mkdir(backup, "p")
 end
-vim.g.cfg = cfg
 vim.g.data = data
-vim.g.cache = cache
 
 -- Bootstrap packer
 local ok = true
@@ -38,27 +34,4 @@ vim.cmd([[runtime autoload/auto_stuff.vim]])
 
 if ok then
   require("custom.plugins")
-
-  vim.cmd([[autocmd BufEnter * :LspStart]])
-  vim.g.tokyonight_style = "night"
-  vim.cmd([[colorscheme tokyonight]])
-
-  -- NOTE(kotto): Kinda defer the loading of the mappings so it doens't take
-  -- 400s to load. With the plugin https://github.com/lewis6991/impatient.nvim
-  -- loading it normaly will take around 100s. Using `vim.schelude` it takes
-  -- 50s
-
-  vim.cmd([[runtime plugin/astronauta.vim]])
-  require("custom.mappings")
-  vim.schedule(function()
-    vim.keymap.nnoremap({
-      "<silent> <leader>",
-      ":<c-u>WhichKey <\\Space><CR>",
-    })
-    vim.keymap.nnoremap({ "<silent> <localleader>", ":<c-u>WhichKey  ,<CR>" })
-  end)
-end
-
-if vim.bo.filetype ~= "lua" then
-  vim.cmd([[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()]])
 end
