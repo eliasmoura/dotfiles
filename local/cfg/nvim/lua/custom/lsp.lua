895fef4785b7162ae1082c9ffefa4e4ea5d4f4c5
Elias Alves Moura <eliamoura.alves@gmail.com>
2022-02-13 14:35:12 +0000

    update(nvim): update to the new vim.keymap.

--- local/cfg/nvim/lua/custom/lsp.lua
+++ local/cfg/nvim/lua/custom/lsp.lua
@@ -1,7 +1,5 @@
 local lspconfig = require("lspconfig")
 vim.cmd([[runtime plugin/astronauta.vim]])
-local nnoremap = vim.keymap.nnoremap
-local inoremap = vim.keymap.inoremap
 
 -- Add additional capabilities supported by nvim-cmp
 local capabilities = vim.lsp.protocol.make_client_capabilities()
@@ -20,43 +18,38 @@ local on_attach = function(client)
   require("lsp-status").on_attach(client)
   local opts = {}
   opts.buffer = true
-  inoremap({ "<c-s>", vim.lsp.buf.signature_help, opts })
-  nnoremap({
-    "<leader>la",
-    require("lspsaga.codeaction").code_action or vim.lsp.buf.code_action,
-    opts,
-  })
-  nnoremap({ "<leader>lf", vim.lsp.buf.formatting })
-  nnoremap({ "<leader>ls", vim.lsp.buf.document_symbol })
-  nnoremap({ "<leader>lh", vim.lsp.buf.hover })
-  nnoremap({ "<leader>lr", vim.lsp.buf.references })
-  nnoremap({
-    "<leader>lR",
-    require("lspsaga.rename").rename or vim.lsp.buf.rename,
-  })
-  nnoremap({ "<leader>ldg", vim.lsp.diagnostic.get_all })
-  nnoremap({ "<leader>lda", vim.lsp.diagnostic.goto_prev })
-  nnoremap({
-    "<c-n>",
-    require("lspsaga.diagnostic").lsp_jump_diagnostic_next
-      or vim.diagnostic.goto_next,
-  })
-  nnoremap({
-    "<c-p>",
-    require("lspsaga.diagnostic").lsp_jump_diagnostic_prev
-      or vim.diagnostic.goto_prev,
-  })
-  nnoremap({
-    "<c-s>",
-    require("lspsaga.diagnostic").show_line_diagnostics
-      or vim.diagnostic.show_line_diagnostics,
-  })
-  nnoremap({ "<M-Q>", vim.lsp.util.set_qflist })
-  nnoremap({ "<M-q>", vim.lsp.util.set_loclist })
-  nnoremap({ "<c-]>", vim.lsp.buf.definition, opts })
+  vim.keymap.set("i", "<c-s>", vim.lsp.buf.signature_help, opts )
+  vim.keymap.set("n", "<leader>la",
+  require("lspsaga.codeaction").code_action or vim.lsp.buf.code_action,
+  opts
+  )
+  vim.keymap.set("n",  "<leader>lf", vim.lsp.buf.formatting )
+  vim.keymap.set("n",  "<leader>ls", vim.lsp.buf.document_symbol )
+  vim.keymap.set("n",  "<leader>lh", vim.lsp.buf.hover )
+  vim.keymap.set("n",  "<leader>lr", vim.lsp.buf.references )
+  vim.keymap.set("n",     "<leader>lR",
+  require("lspsaga.rename").rename or vim.lsp.buf.rename
+  )
+  vim.keymap.set("n",  "<leader>ldg", vim.lsp.diagnostic.get_all )
+  vim.keymap.set("n",  "<leader>lda", vim.lsp.diagnostic.goto_prev )
+  vim.keymap.set("n",     "<c-n>",
+  require("lspsaga.diagnostic").lsp_jump_diagnostic_next
+  or vim.diagnostic.goto_next
+  )
+  vim.keymap.set("n",     "<c-p>",
+  require("lspsaga.diagnostic").lsp_jump_diagnostic_prev
+  or vim.diagnostic.goto_prev
+  )
+  vim.keymap.set("n",     "<c-s>",
+  require("lspsaga.diagnostic").show_line_diagnostics
+  or vim.diagnostic.show_line_diagnostics
+  )
+  vim.keymap.set("n",  "<M-Q>", vim.lsp.util.set_qflist )
+  vim.keymap.set("n",  "<M-q>", vim.lsp.util.set_loclist )
+  vim.keymap.set("n",  "<c-]>", vim.lsp.buf.definition, opts )
 
   if filetype ~= "lua" then
-    nnoremap({ "K", vim.lsp.buf.hover })
+    vim.keymap.set("n",  "K", vim.lsp.buf.hover )
   end
 
   if filetype ~= "zig" and filetype ~= "lua" then
@@ -103,8 +96,6 @@ lspconfig.sumneko_lua.setup({
   on_attach = on_attach,
   cmd = {
     "/usr/bin/lua-language-server",
-    "-E",
-    "/usr/share/lua-language-server/main.lua",
   },
   -- cmd = {'/home/kotto/local/cfg/pacman/makepkg/recipes/sumneko-git/wrapper.sh'},
   -- An example of settings for an LSP server.
