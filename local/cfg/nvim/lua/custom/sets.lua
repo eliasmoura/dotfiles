22c5ec07a68de4f4c5bcea671c2df7d1b78ca32b
Elias Alves Moura <eliamoura.alves@gmail.com>
2022-03-12 13:11:14 +0000

    feat(nvim/sets): user vim.o/bo/wo, highlight the yanked text.

--- local/cfg/nvim/lua/custom/sets.lua
+++ local/cfg/nvim/lua/custom/sets.lua
@@ -1,55 +1,58 @@
-local gopt = vim.g
-local opt = vim.o
-local wopt = vim.wo
-local bopt = vim.bo
-
 vim.g.mapleader = " "
 vim.o.timeout = true
 vim.o.timeoutlen = 600
 vim.g.maplocalleader = "\\"
-opt.updatetime = 50
-wopt.foldmethod = "syntax"
+vim.o.updatetime = 50
+vim.wo.foldmethod = "syntax"
 vim.g.markdown_folding = 1
 vim.g.vim_markdown_folding_disabled = 1
-opt.completeopt = "menuone,noselect" -- ,preview"
-opt.shortmess = opt.shortmess .. "cI"
-opt.cmdheight = 1
--- opt.highlight Normal guibg=none
-opt.exrc = true
-opt.undofile = true -- keep an undo file (undo changes after closing)
-wopt.number = true
-opt.showbreak = "…"
-wopt.cursorline = true
-opt.laststatus = 2
-opt.hidden = true
-opt.wildmode = "longest,full"
-opt.wildoptions = "pum"
-opt.wildignore = "*.pyc,*~,*.o,*.obj"
-opt.suffixesadd = ".py,.pl,.js,.html,.c,.h,.cpp,.hh"
-opt.termguicolors = true
-opt.scrolloff = 8
-opt.vb = true -- visual bell
-bopt.spelllang = "en_us"
-opt.spellsuggest = "best"
-opt.tabstop = 2
-opt.softtabstop = opt.tabstop
-opt.shiftwidth = opt.tabstop
-opt.expandtab = true
-bopt.tabstop = 2
-bopt.softtabstop = bopt.tabstop
-bopt.shiftwidth = bopt.tabstop
-bopt.expandtab = true
-opt.shada = [["10000,'10000,<500,:50,s100,f1,h]]
-wopt.colorcolumn = "80"
-opt.splitright = true
-opt.mouse = "n"
-opt.splitbelow = true
-opt.smartindent = true
-wopt.list = true
-opt.listchars = "tab:> ,trail:-,extends:>,precedes:<,nbsp:+,eol:$"
-opt.cpoptions = opt.cpoptions .. "$" -- put a $ in the end of the change mode
-opt.inccommand = "split"
-gopt.netrw_browsex_viewer = "plumber"
+vim.o.completeopt = "menuone,noselect" -- ,preview"
+vim.o.shortmess = vim.o.shortmess .. "cI"
+vim.o.cmdheight = 1
+-- vim.o.highlight Normal guibg=none
+vim.o.exrc = true
+vim.o.undofile = true -- keep an undo file (undo changes after closing)
+vim.wo.number = true
+vim.wo.rnu = true
+vim.o.showbreak = "…"
+vim.wo.cursorline = true
+vim.o.laststatus = 2
+vim.o.hidden = true
+vim.o.wildmode = "longest,full"
+vim.o.wildoptions = "pum"
+vim.o.wildignore = "*.pyc,*~,*.o,*.obj"
+vim.o.suffixesadd = ".py,.pl,.js,.html,.c,.h,.cpp,.hh"
+vim.o.termguicolors = true
+vim.o.scrolloff = 8
+vim.o.vb = true -- visual bell
+vim.bo.spelllang = "en_us"
+vim.o.spellsuggest = "best"
+vim.o.softtabstop = vim.o.tabstop
+vim.o.shiftwidth = vim.o.tabstop
+vim.o.expandtab = true
+vim.o.tabstop = 2
+vim.bo.tabstop = 2
+vim.bo.softtabstop = vim.bo.tabstop
+vim.bo.shiftwidth = vim.bo.tabstop
+vim.bo.expandtab = true
+vim.o.shada = [["10000,'10000,<500,:50,s100,f1,h]]
+vim.wo.colorcolumn = "80"
+vim.o.splitright = true
+vim.o.mouse = "n"
+vim.o.splitbelow = true
+vim.o.smartindent = true
+vim.wo.list = true
+vim.o.listchars = "tab:> ,trail:-,extends:>,precedes:<,nbsp:+,eol:$"
+vim.o.cpoptions = vim.o.cpoptions .. "$" -- put a $ in the end of the change mode
+vim.o.inccommand = "split"
+vim.o.lazyredraw = true
+
+vim.cmd([[
+augroup highlight_yank
+    autocmd!
+    autocmd TextYankPost * silent! lua vim.highlight.on_yank({timeout = 60})
+augroup END
+]])
 
 -- Disable bultin plugins I don't use.
 vim.g.loaded_getscript = 1
@@ -67,10 +70,11 @@ vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 vim.g.loaded_netrwSettings = 1
 
-vim.g.tar_cmd = "bsdtar"
-vim.g.tar_secure = 1
 vim.g.loaded_gzip = 1
+vim.g.loaded_zip = 1
 vim.g.loaded_zipPlugin = 1
+vim.g.loaded_tar = 1
+vim.g.loaded_tarPlugin = 1
 vim.g.loaded_tohtml = 1
 
 -- Tell vim about the additional file extensions we can now use.
