8146cbd6e7bbb12786e7850437c284c1c58a8a44
Elias Alves Moura <eliamoura.alves@gmail.com>
2022-03-12 13:20:50 +0000

    feat(nvim/lsp): code cleanup and better use of lsp capabilities.

--- local/cfg/nvim/lua/custom/lsp.lua
+++ local/cfg/nvim/lua/custom/lsp.lua
@@ -1,69 +1,60 @@
 local lspconfig = require("lspconfig")
-vim.cmd([[runtime plugin/astronauta.vim]])
 
 -- Add additional capabilities supported by nvim-cmp
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
 
-if vim.bo.filetype ~= "lua" then
-  vim.cmd([[augroup LSP_FORMATING
-  ]])
-  vim.cmd([[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()]])
-end
 vim.cmd([[autocmd BufEnter * :LspStart]])
 
 local on_attach = function(client)
-  require("lspsaga").init_lsp_saga()
-  local filetype = vim.api.nvim_buf_get_option(0, "filetype")
   require("lsp-status").on_attach(client)
   local opts = {}
   opts.buffer = true
-  vim.keymap.set("i", "<c-s>", vim.lsp.buf.signature_help, opts )
-  vim.keymap.set("n", "<leader>la",
-  require("lspsaga.codeaction").code_action or vim.lsp.buf.code_action,
-  opts
+  vim.keymap.set("i", "<c-s>", vim.lsp.buf.signature_help, opts)
+  vim.keymap.set(
+    "n",
+    "<leader>la",
+    require("lspsaga.codeaction").code_action or vim.lsp.buf.code_action,
+    opts
   )
-  vim.keymap.set("n",  "<leader>lf", vim.lsp.buf.formatting )
-  vim.keymap.set("n",  "<leader>ls", vim.lsp.buf.document_symbol )
-  vim.keymap.set("n",  "<leader>lh", vim.lsp.buf.hover )
-  vim.keymap.set("n",  "<leader>lr", vim.lsp.buf.references )
-  vim.keymap.set("n",     "<leader>lR",
-  require("lspsaga.rename").rename or vim.lsp.buf.rename
+  vim.keymap.set("n", "<leader>lf", vim.lsp.buf.formatting)
+  vim.keymap.set("n", "<leader>ls", vim.lsp.buf.document_symbol)
+  vim.keymap.set("n", "<leader>lh", vim.lsp.buf.hover)
+  vim.keymap.set("n", "<leader>lr", vim.lsp.buf.references)
+  vim.keymap.set(
+    "n",
+    "<leader>lR",
+    require("lspsaga.rename").rename or vim.lsp.buf.rename
   )
-  vim.keymap.set("n",  "<leader>ldg", vim.lsp.diagnostic.get_all )
-  vim.keymap.set("n",  "<leader>lda", vim.lsp.diagnostic.goto_prev )
-  vim.keymap.set("n",     "<c-n>",
-  require("lspsaga.diagnostic").lsp_jump_diagnostic_next
-  or vim.diagnostic.goto_next
-  )
-  vim.keymap.set("n",     "<c-p>",
-  require("lspsaga.diagnostic").lsp_jump_diagnostic_prev
-  or vim.diagnostic.goto_prev
-  )
-  vim.keymap.set("n",     "<c-s>",
-  require("lspsaga.diagnostic").show_line_diagnostics
-  or vim.diagnostic.show_line_diagnostics
-  )
-  vim.keymap.set("n",  "<M-Q>", vim.lsp.util.set_qflist )
-  vim.keymap.set("n",  "<M-q>", vim.lsp.util.set_loclist )
-  vim.keymap.set("n",  "<c-]>", vim.lsp.buf.definition, opts )
+  vim.keymap.set("n", "<leader>ldg", vim.diagnostic.get)
+  vim.keymap.set("n", "<c-s>", vim.diagnostic.show)
+  vim.keymap.set("n", "<M-Q>", vim.lsp.util.set_qflist)
+  vim.keymap.set("n", "<M-q>", vim.lsp.util.set_loclist)
+  vim.keymap.set("n", "<c-]>", vim.lsp.buf.definition, opts)
 
-  if filetype ~= "lua" then
-    vim.keymap.set("n",  "K", vim.lsp.buf.hover )
+  if client.resolved_capabilities.document_formatting then
+    vim.cmd([[augroup LSP_FORMATING
+  ]])
+    vim.cmd([[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()]])
   end
 
-  if filetype ~= "zig" and filetype ~= "lua" then
+  if client.resolved_capabilities.code_lens then
+    vim.cmd(
+      [[autocmd BufEnter,CursorHold,InsertLeave <buffer> lua vim.lsp.codelens.refresh()]]
+    )
+  end
+
+  if client.resolved_capabilities.document_highlight then
     vim.cmd(
       [[autocmd CursorHold  <buffer> ++nested lua vim.lsp.buf.document_highlight()]]
     )
     vim.cmd(
       [[autocmd CursorHoldI <buffer> ++nested lua vim.lsp.buf.document_highlight()]]
     )
+    vim.cmd(
+      [[autocmd CursorMoved <buffer> ++nested lua vim.lsp.buf.clear_references()]]
+    )
   end
-
-  vim.cmd(
-    [[autocmd CursorMoved <buffer> ++nested lua vim.lsp.buf.clear_references()]]
-  )
 end
 
 -- Enable some language servers with the additional completion capabilities offered by nvim-cmp
@@ -136,13 +127,17 @@ require("lspconfig").gopls.setup({
   capabilities = capabilities,
   settings = {},
 })
-local lsp_status = require("lsp-status")
-lsp_status.register_progress()
 
-require("lspconfig").zls.setup({
+local lspstatus = require("lsp-status")
+lspstatus.register_progress()
+
+lspconfig.zls.setup({
   on_attach = on_attach,
-  cmd = { "/usr/bin/zls" },
+  cmd = { "zls" },
   capabilities = capabilities,
+  settings = {
+    warn_style = true,
+  },
 })
 
 return lspconfig
