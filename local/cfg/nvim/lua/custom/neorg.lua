d1bdeb6e3b13dbb45a83f4c5b0a9bef8725b3ad9
Elias Alves Moura <eliamoura.alves@gmail.com>
2022-03-13 03:21:47 +0000

    feat(nvim/neorg): stating to realy use neorg.

--- local/cfg/nvim/lua/custom/neorg.lua
+++ local/cfg/nvim/lua/custom/neorg.lua
@@ -2,23 +2,36 @@ local ok, norg = pcall(require, "neorg")
 if not ok then
   return
 end
-print("NORG!")
 
 norg.setup({
   load = {
     ["core.defaults"] = {}, -- Load all the default modules
     ["core.norg.concealer"] = {}, -- Allows for use of icons
     ["core.integrations.treesitter"] = {},
-    ["core.integrations.telescope"] = {},
+    ["core.integrations.telescope"] = {}, -- TODO: check how this works
+    ["core.integrations.nvim-cmp"] = {}, -- TODO: check how this works
+    ["core.norg.journal"] = { config = { workspace = "journal" } },
+    ["core.storage"] = {}, -- TODO: check how this works
     ["core.keybinds"] = { config = { default_keynids = true } },
+    ["core.norg.qol.toc"] = {},
     ["core.norg.completion"] = {
       config = { engine = "nvim-cmp" },
     },
     ["core.norg.dirman"] = { -- Manage your directories with Neorg
       config = {
         workspaces = {
-          main = "~/writings/notes/",
+          notes = "~/writings/notes",
+          journal = "~/writings/gtd/journal",
+          gtd = "~/writings/gtd",
+          example_gtd = "~/writings/example_workspaces/gtd",
         },
+        index = "index.norg",
+      },
+    },
+    ["core.gtd.base"] = {
+      config = {
+        workspace = "gtd",
+        custom_tag_completion = true,
       },
     },
   },
